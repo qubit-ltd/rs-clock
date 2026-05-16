4c2c1351183acde094e72f085a8ba2538d2e8e49
Haixing Hu <starfish.hu@gmail.com>
2026-05-16 22:10:58 +0800

    test(clock): move clock tests into clock module

---
+++ tests/clock/mod.rs
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
+//! Tests for the clock module.
+
+mod clock_tests;
+mod controllable_clock_tests;
+mod mock_clock_tests;
+mod mock_nano_clock_tests;
+mod monotonic_clock_tests;
+mod monotonic_tests;
+mod nano_clock_tests;
+mod nano_monotonic_clock_tests;
+mod nano_monotonic_tests;
+mod system_clock_tests;
+mod system_tests;
+mod zoned_clock_tests;
+mod zoned_tests;
